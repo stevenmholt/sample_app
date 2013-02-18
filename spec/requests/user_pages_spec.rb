7e5c26525d2414cb85c9f915d2838621d7eeb017
Steven Holt <holtstevenmichael@gmail.com>
2013-02-18 10:39:43 -0500

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,7 +24,7 @@ describe "UserPages" do
       it { should have_selector('div.pagination') }
     
       it "should list each user" do
-        User.all.each do |user|
+        User.all[0..2].each do |user|
           page.should have_selector('li', text: user.name)
         end
       end
@@ -58,10 +58,19 @@ describe "UserPages" do
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    
     before { visit user_path(user) }
     
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
+    
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
   
   describe "signup" do
@@ -132,4 +141,6 @@ describe "UserPages" do
       specify { user.reload.email.should == new_email }
     end
   end
+  
+  describe
 end
